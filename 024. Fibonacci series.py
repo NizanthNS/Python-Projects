5569fde555abdc8374cb2b479b8aa99ee053d6b7
Nizanth NS <nizanthns10@gmail.com>
2026-08-15 15:09:37 +0530

    Add files via upload

---
+++ 024. Fibonacci series.py
@@ -0,0 +1,21 @@
+# Fibonacci series
+
+while True:
+    try:
+        n = int(input("Enter the Number of Fibonacci Series: "))
+        if n > 0:
+            break
+        else:
+            print("Please enter a positive integer")
+    except ValueError:
+        print("Invalid input! Please enter a positive integer.")
+
+a = 0
+b = 1
+
+for i in range(n):
+    print(a)
+
+    c = a + b
+    a = b
+    b = c
