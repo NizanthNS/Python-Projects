ffd8a368cfd827c50e37d1f8ca566db137e752af
Nizanth NS <nizanthns10@gmail.com>
2026-08-24 23:19:36 +0530

    Update 005. Python Calculator V1.py

--- 005. Python Calculator V1.py
+++ 005. Python Calculator V1.py
@@ -1,20 +1,43 @@
 # Python Calculator
 
-operator = input("Enter an operator (+ - * /): ")
-num1 = float(input("Enter the 1st number: "))
-num2 = float(input("Enter the 2nd number: "))
+def addition(num1, num2):
+    return num1 + num2
 
-if operator == "+":
-    result = num1 + num2
-    print("The answer is", round(result, 3))
-elif operator == "-":
-    result = num1 - num2
-    print("The answer is", round(result, 3))
-elif operator == "*":
-    result = num1 * num2
-    print("The answer is", round(result, 3))
-elif operator == "/":
-    result = num1 / num2
-    print("The answer is", round(result, 3))
-else:
-    print(f"{operator} is not a valid operator")
+def subtraction(num1, num2):
+    return num1 - num2
+
+def multiplication(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    if num2 == 0:
+        return "Cannot divide by zero"
+    else:
+        return num1 / num2
+
+def main():
+    while True:
+        try:
+            num1 = int(input("Enter the 1st Number: "))
+            num2 = int(input("Enter the 2nd Number: "))
+            break
+        except ValueError:
+            print("Please enter a valid number")
+
+    add = addition(num1, num2)
+    sub = subtraction(num1, num2)
+    mul = multiplication(num1, num2)
+    dvr = division(num1, num2)
+
+    print()
+    print("-----------------------")
+    print("      CALCULATOR       ")
+    print("-----------------------")
+    print(f"Addition       : {add}")
+    print(f"Subtraction    : {sub}")
+    print(f"Multiplication : {mul}")
+    print(f"Division       : {dvr}")
+    print("-----------------------")
+
+if __name__ == "__main__":
+    main()
