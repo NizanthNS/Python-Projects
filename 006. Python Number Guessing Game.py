2786d3a512fc67544b3a6d8c56cb9866acc9fc9e
Nizanth NS <nizanthns10@gmail.com>
2026-08-24 21:02:34 +0530

    Add winning guess limit to number guessing game

--- 006. Python Number Guessing Game.py
+++ 006. Python Number Guessing Game.py
@@ -4,39 +4,57 @@ import random
 
 low = 0
 high = 100
-answer = random.randint(low, high)
-guesses = 0
+w_guess = 6
+
 is_running = True
 
 while is_running:
-    try:
-        guess = int(input(f"Guess a number between {low} and {high}: "))
 
-        guesses += 1
+    answer = random.randint(low, high)
+    guesses = 0
 
-        if guess < low or guess > high:
-            print("That Number is out of range")
+    while guesses < w_guess:
+        try:
+            guess = int(input(f"Guess a number between {low} and {high}: "))
 
-        elif guess < answer:
-            print("That Number is TOO LOW! Try again")
+            if guess < low or guess > high:
+                print("That Number is out of range")
+                continue
 
-        elif guess > answer:
-            print("That Number is TOO HIGH! Try again")
+            guesses += 1
 
-        else:
-            print(f"CORRECT! The Number is {answer}")
-            print(f"You have Guessed {guesses} times")
+            if guess < answer:
+                print("That Number is TOO LOW! Try again")
 
-            play_again = input("Do you want to Play Again (Y/N): ").lower()
+            elif guess > answer:
+                print("That Number is TOO HIGH! Try again")
 
-            if play_again != "y":
-                is_running = False
             else:
-                answer = random.randint(low, high)
-                guesses = 0
+                print(f"CORRECT! The Number is {answer}")
+                print(f"You have Guessed {guesses} times")
 
-    except ValueError:
-        print("INVALID INPUT")
-        print(f"Please Enter a Number between {low} and {high}")
+                if guesses <= w_guess:
+                    print("Congratulations, you won the game!")
+
+                play_again = input("Do you want to Play Again (Y/N): ").lower()
+
+                if play_again != "y":
+                    is_running = False
+
+                break
+
+        except ValueError:
+            print("INVALID INPUT")
+            print(f"Please Enter a Number between {low} and {high}")
+
+    else:
+        print("\nGAME OVER!")
+        print(f"The Number was {answer}")
+        print(f"You used all {w_guess} guesses.")
+
+        play_again = input("Do you want to Play Again (Y/N): ").lower()
+
+        if play_again != "y":
+            is_running = False
 
 print("Thank you for playing")
