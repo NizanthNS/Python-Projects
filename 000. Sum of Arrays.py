9be8d1d924b51424547fb7150db813660ea0a884
Nizanth NS <nizanthns10@gmail.com>
2026-09-19 19:59:33 +0530

    Update 000. Sum of Arrays.py

--- 000. Sum of Arrays.py
+++ 000. Sum of Arrays.py
@@ -11,3 +11,26 @@ print("The Sum of Numbers is", sum_)
 
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 # print(sum(numbers))
+
+# Running Sum
+
+sum_ = 0
+
+for num in range(1, 10):
+    sum_ += num
+
+print(sum_)
+
+# Running Sum - 2
+
+numbers = []
+
+for i in range(1, 5):
+    numbers.append(i)
+
+sum_ = 0
+
+for num in numbers:
+    sum_ += num
+
+print(sum_)
